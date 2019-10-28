1116479b06d729f03bc2b3cf7af74eac83c5dcff
nicolasSilva <silva.nicolas.j@gmail.com>
2019-10-28 14:26:37 +0100

    Readme & program update

--- src/evaluate3dDrug.py
+++ src/evaluate3dDrug.py
@@ -1,32 +1,42 @@
-from keras import *
 import numpy as np
 from keras.models import load_model
-from keras.layers import Activation, Conv3D, Dense, Dropout, Flatten, MaxPooling3D
-from keras.backend import set_image_data_format
 import random as rd
+import sys
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+from scipy import interp
+from itertools import cycle
+import argparse
+
 
 #Function to read and save X and Y data
 def open_dat(fil, typ, nb):
-	encod = {"h" : [1., 0., 0., 0.], "n" : [0., 1., 0., 0.], "s" : [0., 0., 1., 0.], "c" : [0., 0., 0., 1.]}
-	#encod = {"h" : [1., 0., 0.], "n" : [0., 1., 0.], "c" : [0., 0., 1.]}
-	#encod = {"h" : [1., 0.], "n" : [0., 1.]}
+	encod = {"h" : [1, 0, 0, 0], "n" : [0, 1, 0, 0], "s" : [0, 0, 1, 0], "c" : [0, 0, 0, 1]}
 	f = open(fil, "r")
 	f = f.read().splitlines()
 	x_data = []
 	y_data = []
 	dat = []
 	for i in f[-nb:]:
-		dat = np.load("../../data/deepdrug3d_voxel_data/"+i+".npy")
+		dat = np.load("../data/deepdrug3d_voxel_data/"+i+".npy")
 		dat = np.squeeze(dat)
 		x_data.append(dat)
 		y_data.append(encod[typ])
 	return x_data, y_data
 
+
+parser = argparse.ArgumentParser(description='Classification of ligand-binding pocket test')
+parser.add_argument('-f', metavar='Model', type=str, help='Model file .h5', required=True)
+parser.add_argument('-o', metavar='Graph', type=str, help='Output ROC curves')
+args = parser.parse_args()
+model_file = args.f
+save_plot = args.o
+
 #Data to test the model
-(x_heme, y_heme) = open_dat("../../data/heme.list", "h", 200) #Max 596
-(x_nucle, y_nucle) = open_dat("../../data/nucleotid.list", "n", 200) #Max 1553
-(x_steroid, y_steroid) = open_dat("../../data/steroid.list", "s", 69) #Max 69
-(x_control, y_control) = open_dat("../../data/control.list", "c", 200) #Max 1946
+(x_heme, y_heme) = open_dat("../data/heme.list", "h", 300) #Max 596
+(x_nucle, y_nucle) = open_dat("../data/nucleotid.list", "n", 300) #Max 1553
+(x_steroid, y_steroid) = open_dat("../data/steroid.list", "s", 69) #Max 69
+(x_control, y_control) = open_dat("../data/control.list", "c", 300) #Max 1946
 
 #Creating one_hot array
 X = np.array(x_heme + x_nucle + x_steroid + x_control)
@@ -37,12 +47,45 @@ X_Y = list(zip(X,Y))
 rd.shuffle(X_Y)
 X, Y = zip(*X_Y)
 
-oH_x_train = np.array(X)
-oH_y_train = np.array(Y)
+oH_x_test = np.array(X)
+oH_y_test = np.array(Y)
 
 #Load model
-model = load_model('../model_0.93.h5')
-#model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-accuracy = model.evaluate(oH_x_train, oH_y_train)
+model = load_model(model_file)
+model.summary()
+accuracy = model.evaluate(oH_x_test, oH_y_test)
 print(model.metrics_names)
 print(accuracy)
+
+###Courbe ROC par classe
+n_classes = 4
+y_score = model.predict(oH_x_test)
+y_test = oH_y_test
+fpr = dict()
+tpr = dict()
+thresholds_keras = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], thresholds_keras[i] = roc_curve(y_test[:, i], y_score[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Plot all ROC curves
+plt.figure(1)
+colors = cycle(['forestgreen', 'red', 'blue', 'black'])
+classe = ['Hemes','Nucleotides','Steroïdes','Controls']
+for i, color in zip(range(n_classes), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=2,
+             label='Class {0} (AUC = {1:0.2f})'
+             ''.format(classe[i], roc_auc[i]))
+
+plt.plot([0, 1], [0, 1], 'k--', lw=2)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate (1-Specificity)')
+plt.ylabel('True Positive Rate (Sensitivity)')
+plt.title('ROC curves')
+plt.legend(loc="lower right")
+plt.gca().set_aspect('equal',adjustable='box')
+if len(save_plot) > 0:
+	plt.savefig(save_plot)
+plt.show()
